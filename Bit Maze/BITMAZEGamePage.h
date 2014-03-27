ed54309784367a5b3f441001a994b4e280bfdba0
cablej <jackhcable@gmail.com>
2014-03-27 10:50:43 -0500

    Update BITMAZEGamePage.h

--- Bit Maze/BITMAZEGamePage.h
+++ Bit Maze/BITMAZEGamePage.h
@@ -12,6 +12,10 @@
     NSMutableArray* patterns; //Stores patterns, read from patterns.txt
     
     NSMutableArray* gameGrid; //Stores curent game grid, 52 rows, 40 columns, first index is where player is
+
+    BOOL inPattern; //If pattern is currently being generated
+    int currentPatternNumber; //The number of the current pattern
+    int currentPatternRow; //The next row to be generated
 }
 
 @end
