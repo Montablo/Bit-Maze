09e544b37ad848e28ec22fc0598b29190da0efe7
galenqq <galenqq@gmail.com>
2014-03-27 20:32:20 -0500

    Text can be clicked aswell as the square now

--- Bit Maze/BITMAZEHomePage.m
+++ Bit Maze/BITMAZEHomePage.m
@@ -2,8 +2,8 @@
 //  BITMAZEHomePage.m
 //  Bit Maze
 //
-//  Created by Jack on 3/26/14.
-//  Copyright (c) 2014 Montablo Games. All rights reserved.
+//  Created by Galen and Jack on 3/26/14.
+//  Copyright (c) 2014 Montablo. All rights reserved.
 //
 
 #import "BITMAZEHomePage.h"
@@ -23,6 +23,7 @@
         
         SKLabelNode *playLabel = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue Light"];
         playLabel.text = @"Play";
+        playLabel.name = @"playButton";
         playLabel.fontSize = 20;
         playLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
         
