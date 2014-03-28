031f243474e09d76951a7156ad55cbc45be45ee2
cablej <jackhcable@gmail.com>
2014-03-28 16:22:06 -0500

    Added settings page

---
+++ Bit Maze/BITMAZESettingsPage.h
@@ -0,0 +1,13 @@
+//
+//  BITMAZESettingsPage.h
+//  Bit Maze
+//
+//  Created by Jack on 3/28/14.
+//  Copyright (c) 2014 Montablo. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface BITMAZESettingsPage : SKScene
+
+@end
---
+++ Bit Maze/BITMAZESettingsPage.m
@@ -0,0 +1,13 @@
+//
+//  BITMAZESettingsPage.m
+//  Bit Maze
+//
+//  Created by Jack on 3/28/14.
+//  Copyright (c) 2014 Montablo. All rights reserved.
+//
+
+#import "BITMAZESettingsPage.h"
+
+@implementation BITMAZESettingsPage
+
+@end
